827e634717eac747a025bb9520a9310d1d0f164f
Tesseract22 <80269540+Tesseract22@users.noreply.github.com>
2023-01-01 18:02:38 -0600

    Add files via upload

---
+++ PlagueGun/Source/ClassLibrary1/ClassLibrary1/TC_PlagueGunExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+namespace TC_PlagueGun {
+    public class ModExtension_PlagueBullet : DefModExtension
+    {
+        public float addHediffChance = 0.05f;
+        public HediffDef hediffToAdd;
+    }
+}
